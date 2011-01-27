cdf5a5deea526abb21d671e33ea2b9d6fc8b4c92
Lukasz Mielicki <mielicki@gmail.com>
2011-01-27 15:12:51 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,115 @@
+# Intoruction
+
+`fibers-promises` is a small yet powerful library based on [fibers](https://github.com/laverdet/node-fibers).
+
+A promise is just a value container with build-in synchronisation mechanism. Promises allow you to get rid of callbacks clutter.
+
+You may also like [fiberize](http://github.com/lm1/node-fiberize).
+
+# Getting started
+
+    npm install fibers-promise
+    
+This will install node-fibers as well. (Working g++ and node headers are required.)
+
+Then run your code with:
+
+    node-fibers your_file.js
+
+# Example
+
+To fetch an url:
+
+    var http = require('http');
+    var promise = require('fibers-promise');
+    
+    promise.start(function() {
+      
+      var p = promise();
+      
+      http.get({
+          host: 'www.google.com',
+          port: 80,
+          path: '/'
+      }, p);
+      
+      var res = p.get();
+      res.setEncoding('utf8');
+      res.on('data', p);
+      res.on('end', p);
+      
+      var data, chunk;
+      while (chunk = p.get()) {
+        data += chunk;
+      }
+      console.log(data);
+    });
+
+# Interface
+
+    var promise = require('fibers-promise');
+
+### promise()
+
+`promise` is a factory method returning new `Promise` object with the following methods:
+
+  - `Promise.set(value)`
+
+  `set` method sets the promise value and resumes a fiber waiting for it (if any).
+
+  - `Promise(...)`
+
+  `Promise` object is itself a function which when called sets promise value like set method.
+  If more than a single value is provided, promise value is an array containing all provided values.
+  Thus it's possible to pass promise just as a callback to the asynchronous function.
+
+  - `Promise.get()`
+
+  Waits until the promise has a value and return it's value.
+
+  - `Promise.ready()`
+
+  Checks if promise has a value already. Returs boolean value.
+
+  - `Promise.wait()`
+
+  Waits until a promise has a value.
+
+### promise.t()
+
+This is also a factory method similar to `promise()`, but the `get()` method of this promise behaves as follows:
+
+  - Single argument has been provided to `Promise(...)`
+  
+    If the value evaluates to true it's thrown; nothing is returned.
+    
+  - Two arguments were provided to `Promise(...)`
+  
+    If the first value evaluates to true it's thrown, second value is returned.
+    
+  - Three or more arguments were provided to `Promise(...)`
+  
+    If the first value evaluates to true it's thrown; remaining values are returned in form of an Array.
+  
+This type of promise is useful when passed as callback to most node asynchronous methods.
+
+### promise.start(f, [args...])
+
+`start` runs `f` in a new fiber and passes `args` to `f`.
+Returns the value returned by `f`.
+
+### promise.task(f)
+
+Returns a function which will execute `f` in new fiber upon invocation. All the arguments of the returned function are passed directly to `f`. Useful to postpone start of a fiber, or wrap a callback in a fiber.
+
+### promise.waitAny(...)
+
+Waits for multiple promises, the first promise which has a value is returned.
+
+### promise.waitAll(...)
+
+Waits until all provided promises have values.
+
+### promise.sleep(ms)  
+
+Suspends current fiber for `ms` miliseconds. Does not block the event loop, thus other fibers may execute.
---
+++ fibers-promise.js
@@ -0,0 +1,138 @@
+/*
+  Copyright 2011 Lukasz Mielicki <mielicki@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to
+  deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+*/
+
+require('fibers');
+
+function get_current_fiber() {
+  var f = Fiber.current;
+  if (f === undefined) {
+    throw new Error('You need to start a fiber first!');
+  }
+  return f;
+}
+
+function promise() {
+  var fiber = get_current_fiber();
+  var hasValue = false;
+  var value;
+  var multiple = false;
+  var cb = function() {
+    if (arguments.length > 1) {
+      multiple = true;
+      cb.set(Array.prototype.slice.call(arguments));
+    } else {
+      cb.set(arguments[0]);
+    }
+  };
+  cb.set = function(val) {
+    if (hasValue) throw new Error('Promise value already set!');
+    value = val;
+    hasValue = true;
+    if (Fiber.current !== fiber && fiber.started) {
+      fiber.run();
+    }
+  };
+  cb.wait = function() {
+    while (!hasValue) {
+      yield();
+    }
+    return true;
+  };
+  cb.ready = function() {
+    return hasValue;
+  };
+  cb.get = function() {
+    cb.wait();
+    var val = value;
+    hasValue = false;
+    value = undefined;
+    if (cb.__throw) {
+      var err;
+      if (multiple) {
+        err = val.shift();
+        if (val.length <= 1) {
+          val = val[0];
+        }
+      } else {
+        err = val;
+        val = undefined;
+      }
+      if (err) throw err;
+    }
+    return val;
+  };
+  return cb;
+}
+module.exports = promise;
+
+promise.t = function() {
+  var p = promise();
+  p.__throw = true;
+  return p;
+};
+
+promise.start = function(f) {
+  Fiber(function(args) {
+    f.apply(undefined, args);
+  }).run(Array.prototype.slice.call(arguments, 1));
+  return undefined;
+};
+
+promise.task = function(f) {
+  return function() {
+    Fiber(function(args) {
+      f.apply(undefined, args);
+    }).run(arguments);
+  };
+  return undefined;
+};
+
+promise.waitAny = function() {
+  while (true) {
+    for (i = 0; i < arguments.length; ++i) {
+      if (arguments[i].ready()) {
+        return arguments[i];
+      }
+    }
+    yield();
+  }
+}
+
+promise.waitAll = function() {
+  for (i = 0; i < arguments.length; ++i) {
+    arguments[i].wait();
+  }
+  return undefined;
+}
+
+promise.sleep = function(ms) {
+  var wake = false;
+  var fiber = get_current_fiber();
+  setTimeout(function() {
+    wake = true;
+    fiber.run();
+  }, ms);
+  while (!wake) {
+    yield();
+  }
+  return undefined;
+};
